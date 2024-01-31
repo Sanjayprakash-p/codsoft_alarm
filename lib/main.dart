c1e9c23119d371a8f50ca55d18f022da17cd82b7
Sanjay Prakash <sanjayprakashp.eee2021@citchennai.net>
2024-01-31 22:43:15 +0530

    assets

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:alarm/alarm.dart';
 import 'package:flutter/services.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
